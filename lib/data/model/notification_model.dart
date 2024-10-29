0c67c67765616d607b089744703ce0bca46ae76b
nguyenduyhaihauc <nguyenduyhaihauc@gmail.com>
2024-10-29 17:58:29 +0700

    notification model

---
+++ lib/data/model/notification_model.dart
@@ -0,0 +1,13 @@
+class NotificationModel {
+  final String imgAvatar;
+  final String userName;
+  final String content;
+  final String time;
+
+  NotificationModel({
+    required this.imgAvatar,
+    required this.userName,
+    required this.content,
+    required this.time
+  });
+}
