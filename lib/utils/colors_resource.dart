6724ed00a1f8167511c5a860cd301cd3dcb54e60
nguyenduyhaihauc <nguyenduyhaihauc@gmail.com>
2024-11-04 17:39:11 +0700

    class tron mau 4/11/2024

---
+++ lib/utils/colors_resource.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorResource {
+  static const LinearGradient blueButton = LinearGradient(
+      colors: [
+        Colors.blueAccent, Colors.lightBlueAccent
+      ],
+    begin: Alignment.centerLeft,
+    end: Alignment.bottomRight
+  );
+}
