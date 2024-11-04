6c39e2193106511a33b6a946830f6a83abc4598b
nguyenduyhaihauc <nguyenduyhaihauc@gmail.com>
2024-11-04 09:01:56 +0700

    list node chuc nang co the xem them va an 4/11/2024

--- lib/screen/tab_screen/menu_screen.dart
+++ lib/screen/tab_screen/menu_screen.dart
@@ -1,10 +1,85 @@
 import 'package:flutter/material.dart';
 
-class MenuScreen extends StatelessWidget {
+class MenuScreen extends StatefulWidget {
   const MenuScreen({super.key});
 
   @override
+  State<MenuScreen> createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  bool showAll = false;
+
+  // List item to display
+  List<Map<String, dynamic>> items = [
+    {'image': 'images/icon_time_clock.png', 'text': 'Kỷ niệm'},
+    {'image': 'images/icon_saved.png', 'text': 'Đã lưu'},
+    {'image': 'images/icon_group.png', 'text': 'Nhóm'},
+    {'image': 'images/icon_video.png', 'text': 'Video'},
+    {'image': 'images/icon_marketplace.png', 'text': 'Marketplace'},
+    {'image': 'images/icon_friends.png', 'text': 'Bạn bè'},
+    {'image': 'images/icon_feed.png', 'text': 'Bảng feed'},
+    {'image': 'images/icon_dating.png', 'text': 'Hẹn hò'},
+    {'image': 'images/icon_avatar.png', 'text': 'Avatar'},
+    {'image': 'images/icon_play_game.png', 'text': 'Chơi game'},
+    {'image': 'images/icon_tag.png', 'text': 'Lướt thấy'},
+    {'image': 'images/icon_reels.png', 'text': 'Reels'},
+    {'image': 'images/icon_box.png', 'text': 'Sinh nhật'},
+    {'image': 'images/icon_event.png', 'text': 'Sự kiện'},
+  ];
+
+  void _handleButtonPress(String text) {
+    switch (text) {
+      case 'Kỷ niệm':
+        print('Ky niem da duoc nhan');
+        break;
+      case 'Đã lưu':
+        print('Da luu da duoc nhan');
+        break;
+      case 'Nhóm':
+        print('Nhom da duoc nhan');
+        break;
+      case 'Video':
+        print('Video da duoc nhan');
+        break;
+      case 'Marketplace':
+        print('Marketplace da duoc nhan');
+        break;
+      case 'Bạn bè':
+        print('Ban be da duoc nhan');
+        break;
+      case 'Bảng feed':
+        print('Bang feed da duoc nhan');
+        break;
+      case 'Hẹn hò':
+        print('Hen ho da duoc nhan');
+        break;
+      case 'Avatar':
+        print('Avatar da duoc nhan');
+        break;
+      case 'Chơi game':
+        print('Choi game da duoc nhan');
+        break;
+      case 'Lướt thấy':
+        print('Luot thay da duoc nhan');
+        break;
+      case 'Reels':
+        print('Reels da duoc nhan');
+        break;
+      case 'Sinh nhật':
+        print('Sinh nhat da duoc nhan');
+        break;
+      case 'Sự kiện':
+        print('Su kien da duoc nhan');
+        break;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    List<Map<String, dynamic>> displayedItems =
+    showAll ? items : items.sublist(0, 6);
+
     return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -44,13 +119,74 @@ class MenuScreen extends StatelessWidget {
                   'Shorctus',
                 style: TextStyle(
                   fontSize: 14,
-                  fontWeight: FontWeight.w400,
+                  fontWeight: FontWeight.w500,
                   color: Colors.black87
                 ),
               )
           ),
         // Cac nut chuc nang
-
+          Container(
+            margin: EdgeInsets.only(left: 20, top: 10, right: 20, bottom: 15),
+            child: GridView.builder(
+              shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                  childAspectRatio: 3,
+                  mainAxisExtent: 70
+                ),
+                itemCount: displayedItems.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(8)
+                    ),
+                    child: GestureDetector(
+                      onTap: () => _handleButtonPress(displayedItems[index]['text']),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset(displayedItems[index]['image'], width: 24, height: 24,),
+                          SizedBox(height: 5,),
+                          Text(displayedItems[index]['text'], style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black87
+                          ),)
+                        ],
+                      ),
+                    ),
+                  );
+                }
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      showAll = !showAll;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey[200],
+                    foregroundColor: Colors.black87,
+                    // elevation: 5,// Do bong
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6)
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 35),
+                  ),
+                  child: Text(showAll ? 'Ẩn bớt' : 'Xem thêm', style: TextStyle(fontWeight: FontWeight.w600),)
+              ),
+            ),
+          ),
         ],
       ),
     );
